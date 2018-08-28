83398c336ea141b45f3a651f39dab684008312f7
Raymond Arceneaux <raymond.techcoder@gmail.com>
2018-08-28 14:07:21 -0500

    add objects js

---
+++ objectchallenge.js
@@ -0,0 +1,73 @@
+let elizabethsanger = {
+
+    congressionalDistrict :5,
+
+    platformStatement : [
+        {statement:"keep jobs in us", category:"Jobs"},
+
+        {statement: "Gun Violence", category:"Crime and Enforcement"},
+
+        {statement:"No more taxes", category:"Taxes"},
+
+    ],
+
+
+    urlDonation:  "www.amazon.com" ,
+
+
+    calendarOfEvents = [
+        {date : '08/25/2018', title:"rally with Trump", description:"Drain the swamp"},
+
+        {date : '08/26/2018', title:"meet with VD", description:"Meet with Vets"},
+    ] ,
+
+    volunteerInformation: [
+        {
+            name: 'Jay',
+            address:'100 Elm St',
+            email:'igotmoney@com.',
+            phone:'4132287656',
+            availability:'12-5',
+            activities:'world tour'
+
+        },
+
+        {
+            name: 'Mario',
+            address:'400 Elm St',
+            email:'tntitans@com.',
+            phone:'4132287659',
+            availability:'1-5',
+            activities:'everyday except game day'
+
+
+        }
+    ],
+
+biography: "I am for the people",
+
+
+
+
+
+imageGallery [
+
+    {
+imageUrl: "https://www.worldatlas.com/r/w728-h425-c728x425/upload/8e/6f/eb/shutterstock-61933045.jpg",
+description: "usa",
+type: "red white and blue"
+
+    },
+
+    {
+    imageUrl: "https://imageserver-bisnow1.netdna-ssl.com/9-VQ4kDgPljmo9T-GFiJblD3SNE=/0x0/publisher/59fe162beda66_Screen_Shot_2017_11_04_at_3_32_51_PM.png",
+    description: "DC",
+    type: "Capitol Hill"
+    }
+],
+
+missionStatement: "Make America Great Again",
+
+voterRegUrl: 'www.facebook.gov'
+
+};
